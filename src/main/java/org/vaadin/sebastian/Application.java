c7460777db1a680ec261acca2604be999346a102
Sebastian Kuehnau <seb@vaadin.com>
2021-10-20 11:26:59 +0200

    remove DataGenerator and update to 22.0.0alpha8

--- src/main/java/org/vaadin/sebastian/Application.java
+++ src/main/java/org/vaadin/sebastian/Application.java
@@ -1,9 +1,17 @@
 package org.vaadin.sebastian;
 
+import org.ajbrown.namemachine.NameGenerator;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.vaadin.sebastian.entity.Person;
+import org.vaadin.sebastian.service.PersonService;
+
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 /**
  * The entry point of the Spring Boot application.
@@ -14,6 +22,31 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    final static Logger logger = Logger.getLogger("InitDataService");
+    protected static final int COUNT = 100000;
+
+    @Bean
+    public CommandLineRunner createDemoDataIfNeeded(PersonService personService) {
+        return args -> {
+            if (personService.count() >= COUNT)
+                return ;
+
+            logger.info("add new data");
+
+            var generator = new NameGenerator();
+
+            var names = generator.generateNames(COUNT);
+
+            var personList = names.stream()
+                    .map(name -> Person.Builder.create(name))
+                    .collect(Collectors.toSet());
+
+            personService.saveAll(personList);
+
+            logger.info("generated " + personService.count() + " items");
+        };
     }
 
 }
